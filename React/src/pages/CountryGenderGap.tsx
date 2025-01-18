7314ec969c0e6ca233ef9209da15b249112657e0
jessie-2023 <jessie.xie2023@gmail.com>
2025-01-18 08:46:11 +0000

    byebye chartjs

--- React/src/pages/CountryGenderGap.tsx
+++ React/src/pages/CountryGenderGap.tsx
@@ -1,38 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Gap from "../models/Gap";
 import { useParams } from "react-router-dom";
-import { Line, getElementAtEvent } from "react-chartjs-2";
+import { Chart } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
   LineElement,
   PointElement,
-  Point,
+  BarElement,
   Title,
   Tooltip,
   Legend,
-  TooltipItem,
+  LegendItem,
 } from "chart.js";
-import { useNavigate } from "react-router-dom";
-import chroma from "chroma-js";
 
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+import * as d3  from 'd3-scale';
+
+ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, BarElement, Title, Tooltip, Legend);
+
+// Helper function for generating color gradients, no need to DEFINE it in every render
+const generateMultiStopGradient = (colors: string[], steps: number) => {
+    const colorScale = d3.scaleLinear()
+      .domain(colors.map((_, i) => i / (colors.length - 1))) 
+      .range(colors); 
+    return Array.from({ length: steps }, (_, i) => colorScale(i / (steps - 1)));
+  };
+
 
 export default function CountryGenderGap()
 {
+    const chartRef = useRef(null);
     const { id } = useParams();
-    const [countryGaps, setCountryGaps] = useState<Gap[]>([]);
-    const [lineData, setLineData] = useState({
+    const [countryData, setCountryData] = useState<Gap[]>([]);
+    const [mixedChartData, setMixedChartData] = useState({
         labels: [],
-        datasets: [{
-            label: "Gender Gap Over Time",
-            data: [],
-            borderColor: "rgba(75, 192, 192, 1)",
-            backgroundColor: "rgba(75, 192, 192, 0.2)",
-            tension: 0.1,
-        },]
-    })
+        datasets: [] as unknown[], //reducing redundancy & allow properties varies across chart types
+    });
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<boolean>(false);
 
@@ -40,46 +44,56 @@ export default function CountryGenderGap()
         fetch(`http://localhost:5287/gender-gap/country/${id}`)
         .then(response => response.json())
         .then(data => {
-            setCountryGaps(data);
+            setCountryData(data);
 
-            const colors = chroma.scale("Set3").colors(4);
-            const colorsLess = colors.map(color => chroma(color).alpha(0.5).css()); 
-            setLineData({
+            const colors = generateMultiStopGradient(["#658cbb", '#d1768f'], 4);
+
+            setMixedChartData({
                 labels: data.map((item: Gap) => item.year),
                 datasets: [{
-                        label: "General",
-                        data: data.map((item: Gap) => item.generalGap),
-                        borderColor: colors[0],
-                        backgroundColor: colorsLess[0],
-                        tension: 0.1,
-                    },
-                    {
+                        type: 'line',
                         label: "Health and Survival",
                         data: data.map((item: Gap) => item.healthGap),
-                        borderColor: colors[1],
-                        backgroundColor: colorsLess[1],
-                        tension: 0.1,
+                        borderColor: colors[0], //line
+                        backgroundColor: colors[0], //line filling 
+                        //pointBorderColor, pointBackgroundColor match by default
+                        borderWidth: 3, //thickness
+                        //pointRadius match
+                        tension: 0, //no curving
                     },
                     {
+                        type: 'line',
                         label: "Educational Attainment",
                         data: data.map((item: Gap) => item.educationGap),
-                        borderColor: colors[2],
-                        backgroundColor: colorsLess[2],
-                        tension: 0.1,
+                        borderColor: colors[1],
+                        backgroundColor: colors[1],
+                        borderWidth: 3,
+                        tension: 0,
                     },
                     {
+                        type: 'line',
                         label: "Economic Participation and Opportunity",
                         data: data.map((item: Gap) => item.economicalGap),
-                        borderColor: colors[3],
-                        backgroundColor: colorsLess[3],
-                        tension: 0.1,
+                        borderColor: colors[2],
+                        backgroundColor: colors[2],
+                        borderWidth: 3,
+                        tension: 0,
                     },
                     {
+                        type: 'line',
                         label: "Political Empowerment",
                         data: data.map((item: Gap) => item.politicalGap),
-                        borderColor: colors[4],
-                        backgroundColor: colorsLess[4],
-                        tension: 0.1,                
+                        borderColor: colors[3],
+                        backgroundColor: colors[3],
+                        borderWidth: 3,
+                        tension: 0,              
+                    },
+                    {
+                        type: 'bar',
+                            label: "General Index - average of the four",
+                            data: data.map((item: Gap) => item.generalGap),
+                            backgroundColor: "#87ceeb",
+                            barThickness: 20,
                     },],
             });
 
@@ -89,14 +103,31 @@ export default function CountryGenderGap()
     }, [id]);
 
 
-    const lineOption = {
+    const chartOption = {
         maintainAspectRatio: false,
         responsive:true,
         plugins: {
             title: {
                 display: true,
-                text: "How much of the gender gap toward full parity has been closed",
+                text: "The gender gap index is the average of parity index across the four dimensions",
             },
+            legend: {
+                labels: {
+                    generateLabels: (chart) => {
+                        const chartInstance = chartRef.current?.chartInstance;
+                        if (chartInstance) {
+                            const labels = Chart.defaults.plugins.legend.labels.generateLabels(chart);
+                            labels.forEach((label) => {
+                            const dataset = chart.data.datasets[label.datasetIndex];
+                            if (dataset.type === "line") {
+                                labels.usePointStyle = true;
+                            } 
+                        })
+                        }
+                        
+                    }
+                }
+            }
         },
     }
 
@@ -104,14 +135,20 @@ export default function CountryGenderGap()
     if (error) return <p>Error loading data.</p>;
 
     return (
-        <div>
-            <h3>Gender gap</h3>           
-            {countryGaps.length > 0 && ((<>
+        <div>           
+            {countryData.length > 0 && (<div>
+            <h3>{countryData[0].countryName}, {countryData[0].year} - {countryData[countryData.length - 1].year}</h3>
+            <h4>Region: {countryData[0].regionName}</h4>
+
             <div className="chart-container">
-                <Line data = {lineData} options={lineOption} />
+                <Chart
+                    ref={chartRef}
+                    type="line" 
+                    data = {mixedChartData} 
+                    options={chartOption} 
+                />
             </div>
-            </>))}
+            </div>)}
         </div>
     )
-
 }
